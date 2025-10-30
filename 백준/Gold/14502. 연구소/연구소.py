7b08bce57e72b35ef3303041f3f7578cc6f971b4
김주연 <kimjuyeon02@gmail.com>
2025-10-31 00:26:49 +0900

    [Gold IV] Title: 연구소, Time: 4188 ms, Memory: 36068 KB -BaekjoonHub

---
+++ 백준/Gold/14502. 연구소/연구소.py
@@ -0,0 +1,39 @@
+import itertools, copy
+from collections import deque
+
+dr = [-1, 1, 0, 0]
+dc = [0, 0, -1, 1]
+
+N, M = map(int, input().split())
+grid = [list(map(int, input().split())) for _ in range(N)]
+
+max_zero_count = 0
+zero_check = []
+two_check = []
+
+for i in range(N):
+    for j in range(M):
+        if grid[i][j] == 0:
+            zero_check.append((i, j))
+        elif grid[i][j] == 2:
+            two_check.append((i, j))
+
+for walls in itertools.combinations(zero_check, 3):
+    temp = copy.deepcopy(grid)
+
+    for r, c in walls:
+        temp[r][c] = 1
+
+    queue = deque(two_check)
+    while queue:
+        r, c = queue.popleft()
+        for i in range(4):
+            nr, nc = r + dr[i], c + dc[i]
+            if 0 <= nr < N and 0 <= nc < M and temp[nr][nc] == 0:
+                temp[nr][nc] = 2
+                queue.append((nr, nc))
+
+    zero_count = sum(row.count(0) for row in temp)
+    max_zero_count = max(max_zero_count, zero_count)
+
+print(max_zero_count)
